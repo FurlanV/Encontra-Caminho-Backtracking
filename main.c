2f85a6e62cb076bb02bd194a2aa0f51ca7785636
Vinicius Furlan <vinicius.cfurlan@gmail.com>
2016-10-22 12:15:12 -0200

    Add files via upload

--- main.c
+++ main.c
@@ -17,34 +17,53 @@
  * 
  */
 int main(int argc, char** argv) {
-    
+
     InfoLocal local;
     int marca;
     char nomeArquivo[30];
     int arq = 1;
-    
-    printf("Digite o nome do arquivo: ");
-    scanf("%s",nomeArquivo);
-    getchar();
-    while(arq){
-        montaMapa(&local, nomeArquivo);
-        imprimirMapa(local);
-        movimenta_cachorro(&local, local.posicaoInicialX, local.posicaoInicialY, &marca);
-        printf("O cachorro se movimentou %d vezes\n\n",local.movimentacoes);
-        printf("Deseja carregar um novo mapa ?\n0- Nao | 1- Sim\n");
-        scanf("%d",&arq);
-        
-        while(arq < 0 || arq > 1){
-            printf("0- Nao | 1- Sim");
-            scanf("%d",&arq);
-        }
-        
-        if(arq == 1){
-            printf("Digite o nome do arquivo: ");
-            scanf("%s",nomeArquivo);
+    int opc;
+
+
+    while (arq) {
+
+        printf("Digite o nome do arquivo: ");
+        scanf("%s", nomeArquivo);
+        getchar();
+
+        if (montaMapa(&local, nomeArquivo)) {
+
+            printf("1- Resolver caminho | 2- Resolver caminho em modo análise\n");
+            scanf("%d", &opc);
+
+            switch (opc) {
+                case 1:
+                    imprimirMapa(local);
+                    printf("\nVa apertando 'Enter' para ir para o próximo passo\n");
+                    getchar();
+                    movimenta_cachorro(&local, local.posicaoInicialX, local.posicaoInicialY, &marca);
+                    break;
+                case 2:
+                    imprimirMapa(local);
+                    printf("\nVa apertando 'Enter' para ir para o próximo passo\n");
+                    getchar();
+                    movimenta_cachorro(&local, local.posicaoInicialX, local.posicaoInicialY, &marca);
+                    printf("O cachorro se movimentou %d vezes e chegou na linha %d - coluna %d\n\n", local.movimentacoes, local.linhaAtual, local.ColunaAtual);
+                    break;
+                default:
+
+                    break;
+            }
+
+            printf("Deseja carregar um novo mapa ?\n0- Nao | 1- Sim\n");
+            scanf("%d", &arq);
+
+            while (arq < 0 || arq > 1) {
+                printf("0- Nao | 1- Sim");
+                scanf("%d", &arq);
+            }
         }
     }
-    
     return (EXIT_SUCCESS);
 }
 
